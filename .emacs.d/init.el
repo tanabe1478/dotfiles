102b9c44f0ec59ad6725783ddc77590055ed0517
田辺信之 <tanabenobuyuki@tanabenobuyukinoMacBook-puro.local>
2018-09-25 19:43:25 +0900

    最低限必要な拡張機能とcompanyの導入で補完機能が効くようにした。

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -65,11 +65,6 @@
 ;;                         (setq stops (cons (+ 4 (car stops)) stops)))
 ;;                       (nreverse stops)))
 
-;; 自動セーブとバックアップファイルを無効にする
-(setq backup-inhibited t)
-(setq make-backup-files nil)
-(setq auto-save-default nil)
-
 ;; Mac の文字コードの設定
 (set-language-environment "Japanese")
 (require 'ucs-normalize)
@@ -180,9 +175,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(company-idle-delay nil)
  '(package-selected-packages
    (quote
-    (inf-ruby rbenv ruby-block ruby-electric ruby-mode helm php-mode))))
+    (robe company undo-tree undohist moccur-edit color-moccur inf-ruby rbenv ruby-block ruby-electric ruby-mode helm php-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -253,6 +249,48 @@ If INDENT is `multi-char', that means indent multi-character
 (define-key global-map [?¥] [?\\])
 ;;(define-key html-mode-map [?¥] [?\\])
 (define-key isearch-mode-map [?¥] '(lambda () (interactive)(isearch-process-search-char ?\\)))
+
+(add-to-list 'exec-path "/usr/local/bin")
+(add-to-list 'exec-path "/opt/local/bin")
+
+;; リージョン内の行数と文字数をモードラインに表示する
+(defun count-lines-and-chars()
+  (if mark-active
+      (format "(%dlines, %dchars) "
+              (count-lines (region-beginning) (region-end))
+              (- (region-end) (region-beginning)))
+    ""))
+
+;; default-mode-line-formatがなくなったので修正が必要
+;;(add-to-list 'default-mode-line-format
+;;             '(:eval (count-lines-and-chars)))
+
+;; paren-mode 対応するカッコを強調して表示する
+(setq show-paren-delay 0) ;;表示までの秒数
+(show-paren-mode t)
+;; parenのスタイル expressionは括弧内も強調表示
+(setq show-paren-style 'expression)
+;; フェイスを変更する
+;; (set-face-background 'show-paren-match-face nil)
+;; (set-face-underline-p 'show-paren-match-face "darkgreen")
+
+;; 更新されたファイルを自動的に読み込みなおｓ
+(global-auto-revert-mode t)
+
+;; ファイルが#! から始まる場合、+xを付けて保存する
+(add-hook 'after-save-hook
+          'executable-make-buffer-file-executable-if-script-p)
+
+;; emacs-lisp-modeのフックをセット
+;; カーソル位置にあるElisp関数や変数の情報をエコーエリアに表示させる
+(defun elisp-mode-hooks ()
+  "lisp-mode-hooks"
+  (when (require 'eldoc nil t)
+    (setq eldoc-idle-delay 0.2)
+    (setq eldoc-echo-area-use-multiline-p t)
+    (turn-on-eldoc-mode)))
+
+(add-hook 'emacs-lisp-mode-hook 'elisp-mode-hooks)
 
 
 ;; ----------------------------------------以下拡張周りの設定--------------------------------------------
@@ -347,6 +385,10 @@ If INDENT is `multi-char', that means indent multi-character
 (require 'rbenv)
 (global-rbenv-mode)
 
+;; rbenvパス設定
+(setenv "PATH" (concat (expand-file-name "~/.anyenv/envs/rbenv/shims/ruby") (getenv "PATH")))
+
+
 (require 'ruby-block)
 (ruby-block-mode t)
 
@@ -386,3 +428,74 @@ If INDENT is `multi-char', that means indent multi-character
 ;;; リージョンを削除できるように
 (delete-selection-mode t)
 
+;; color-mooccurの設定 occur-by-moccurはカレントバッファを検索する
+(when (require 'color-moccur nil t)
+  ;; M-oにoccur-by-moccurを割り当て
+  (define-key global-map (kbd "M-o") 'occur-by-moccur)
+  ;; スペース区切りでAND検索
+  (setq moccur-split-word t)
+  ;; ディレク折検索の時に除外するファイル
+  (add-to-list 'dmoccur-exclusion-mask "\\.DS_Store")
+  (add-to-list 'dmoccur-exclusion-mask "^#.+#$"))
+
+
+;; 検索したものを一気に置換できるmoccur-editに関する設定
+(require 'moccur-edit nil t)
+
+;;moccur-editについて
+;; 現環境ではmoccurによる検索を実行したあとに*Moccur*バッファ上でrを押すとmoccur-editモードになる
+;; C-x kで変更が破棄できる
+
+;; moccur-edit-finish-editと同時にファイルを保存する
+(defadvice moccur-edit-change-file
+    (after save-after-moccur-edit-buffer activate)
+  (save-buffer))
+
+
+;; undohist
+(when (require 'undohist nil t)
+  (undohist-initialize))
+
+;; undotree
+(when (require 'undo-tree nil t)
+  ;; C-'にリドゥを割り当てる
+  ;; (define-key global-map (kbd "C-'") 'undo-tree-redo)
+  ;; 通常はC-x uでundo-tree-visualize
+  ;; 戻りたいポイントでqで抜けられる
+  ;; tで樹形図を時間表示と切り替える
+  (global-undo-tree-mode))
+
+
+;; settings about company
+(require 'company)
+(global-company-mode)
+(setq company-idle-delay 0)
+(setq company-minimum-prefix-length 2)
+(setq company-selection-wrap-around t ) ;; 候補の一番下でさらに下に行こうとすると一番上に戻る
+(setq company-auto-expand t)
+(setq company-transformers '(company-sort-by-backend-importance)) ;; ソート順
+(setq company-idle-delay 0) ; 遅延なしにすぐ表示
+(setq company-minimum-prefix-length 1) ; 何文字打つと補完動作を行うか設定
+(setq completion-ignore-case t)
+(setq company-dabbrev-downcase nil)
+(define-key company-active-map (kbd "M-n") nil)
+(define-key company-active-map (kbd "M-p") nil)
+(define-key company-active-map (kbd "C-n") 'company-select-next)
+(define-key company-active-map (kbd "C-p") 'company-select-previous)
+(define-key company-active-map (kbd "C-h") nil)
+(define-key company-active-map (kbd "C-s") 'company-filter-candidates) ;; C-sで絞り込む
+
+
+;; 補完機能
+;; robe-modeの有効化とcompanyとの連携
+(add-hook 'ruby-mode-hook 'robe-mode)
+(autoload 'robe-mode "robe" "Code navigation, documentation lookup and completion for Ruby" t nil)
+(eval-after-load 'company
+  '(push 'company-robe company-backends))
+
+(add-hook 'ruby-mode-hook (lambda()
+      (company-mode)
+      (global-set-key (kbd "C-M-i") 'company-complete)
+      (define-key company-active-map [tab] 'company-complete-selection) ;; TABで候補を設定
+      (define-key emacs-lisp-mode-map (kbd "C-M-i") 'company-complete) ;; 各種メジャーモードでも C-M-iで company-modeの補完を使う
+      ))
